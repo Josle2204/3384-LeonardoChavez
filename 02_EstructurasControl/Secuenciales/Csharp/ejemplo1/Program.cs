5542bb1262532d45f05007d9a6547f68a5d3fc4d
leonardochavez <leonardo.chavroj@gmail.com>
2025-09-13 20:22:06 -0500

    Agregando Secuenciales//CShard//ejemplo 1 y ejemplo 2

---
+++ 02_EstructurasControl/Secuenciales/Csharp/ejemplo1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejemplo 1 C# -- Secuenciales
+            //Calcular area de un triángulo
+
+            //Definir
+            int bas, altura;
+            float area;
+
+            //Ingresar valores
+            Console.WriteLine("Ingrese la base de su triangulo");
+            bas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la altura de su triangulo");
+            altura = int.Parse(Console.ReadLine());
+
+            //Formula
+            area = (float)(bas * altura) / 2;
+
+            //Impresión
+            Console.WriteLine($"Su area del triangulo es {area}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02_EstructurasControl/Secuenciales/Csharp/ejemplo2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejemplo 2 C# -- Secuenciales
+            //Calcular area de un terrero rectangular
+
+            //Definir
+            float ancho, largo, area;
+      
+            //Ingresar Valores
+            Console.WriteLine("Ingrese su ancho del terreno en m:");
+            ancho = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su largo del terreno en m:");
+            largo = int.Parse(Console.ReadLine());
+
+            //Formula
+            area = ancho * largo;
+
+            //Impresion
+            Console.WriteLine($"Su area del terreno es de {area} m2");
+
+            Console.ReadKey();
+        }
+    }
+}
