ac17c1d19bf303aeac6c593b7b20a801159f68b1
leonardochavez <leonardo.chavroj@gmail.com>
2025-09-13 21:09:41 -0500

    02_//Secuenciales//Python//ejemplo1 -- Culminacion de ejemplo1

---
+++ 02_EstructurasControl/Secuenciales/Python/ejemplo1.py
@@ -0,0 +1,16 @@
+#Ejemplo 1 -- Secuenciales
+#Calcular area de un triangulo
+
+print("Ingrese la base de su triangulo:")
+bas = int(input())      #'bas' DECLARADA como entera//Ingresando el valor de la base
+
+print("Ingrese la altura de su triangulo")
+altura = int(input())   #'altura' DECLARADA como entera//Ingresando el valor de la altura
+
+#Forumula
+area = (bas * altura)/2
+
+#Impresion
+print(f"Su area del triangulo es {area}")
+
+input()     #pausa para que no se cierre automaticamente3
