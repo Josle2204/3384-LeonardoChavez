93f86490b0fe17f7df2ec35ad5d9d3015075ab2a
leonardochavez <leonardo.chavroj@gmail.com>
2025-09-14 23:18:49 -0500

    02_//Condicionales//Simples//Csharp// Ejemplo 1 -- Agregando el ejemplo 1

---
+++ 02_EstructurasControl/Condicionales/Simples/CShard/ejemplo1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Condicionales -- Simples // Ejemplo 1
+            //Hacer descuento del 10% solo si la compra es >= a S/1000
+
+            //Definir
+            float valor;
+
+            //Ingresar valor
+            Console.WriteLine("Ingrese el valor de su compra:");
+            valor = float.Parse(Console.ReadLine());
+
+            //Condicional Simple y formula
+
+            if (valor >= 1000)
+            {
+                valor = (float)(valor * 0.90);
+
+                Console.WriteLine($"Tiene un descuento del 10% y deberá pagar S/{valor}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
