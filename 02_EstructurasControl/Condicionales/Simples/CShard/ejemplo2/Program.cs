3052780ad17ab5252b85077bb598ed884c7ac17e
leonardochavez <leonardo.chavroj@gmail.com>
2025-09-14 23:19:27 -0500

    02_//Condicionales//Simples//Csharp// Ejemplo 2 -- Agregando el ejemplo 2

---
+++ 02_EstructurasControl/Condicionales/Simples/CShard/ejemplo2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Condicionales -- Simples // Ejemplo 2
+            //Saludar solo si el usuario ingresado es 'admin'
+
+            //Definir
+            string user;
+
+            //Ingresar valor
+            Console.WriteLine("Ingrese el nombre de usuario:");
+            user = Console.ReadLine();
+
+            //Condicional Simple
+
+            if (user == "admin")
+            {
+                Console.WriteLine("¡Tenga un bonito dia!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
