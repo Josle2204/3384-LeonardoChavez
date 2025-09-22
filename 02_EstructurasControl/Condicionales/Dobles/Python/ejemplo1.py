ea4ccba6baaf7f8f9441ae06f1b4f83abf8f7d1f
leonardochavez <leonardo.chavroj@gmail.com>
2025-09-21 20:06:10 -0500

    02_//Condicionales//Dobles//Python//ejmplo1.py >> Culminacion de ejemplo1.py

--- 02_EstructurasControl/Condicionales/Dobles/Python/ejemplo1.py
+++ 02_EstructurasControl/Condicionales/Dobles/Python/ejemplo1.py
@@ -1,3 +1,14 @@
 #Mostrar si el numero ingresado es par o impar
         
-            //Definir y solicitar valor del numero entero
+#Definir y solicitar valor del numero entero:
+print("Ingrese un numero entero: ")
+num = int(input())
+
+#Usando condicional doble:
+if num % 2 == 0:
+    print(f"Su numero {num} es par")
+else:
+    print(f"Su numero {num} es impar")
+
+#Pausa para que no se cierre
+input()
