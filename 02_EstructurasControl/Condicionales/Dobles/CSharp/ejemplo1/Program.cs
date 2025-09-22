14e9f842016b8e92dd9fef0e19f48769089e2d4e
leonardochavez <leonardo.chavroj@gmail.com>
2025-09-21 19:58:15 -0500

    02_//Condicionales//Dobles//CSharp >> Agregando el ejemplo 1

---
+++ 02_EstructurasControl/Condicionales/Dobles/CSharp/ejemplo1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mostrar si el numero ingresado es par o impar
+        
+            //Definir y solicitar valor del numero entero
+            Console.WriteLine("Ingrese un numero entero: ");
+            int num = int.Parse(Console.ReadLine());
+
+            //Usando condicional doble
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"Su numero {num} es par");
+            }
+            else
+            {
+                Console.WriteLine($"Su numero {num} es impar");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
