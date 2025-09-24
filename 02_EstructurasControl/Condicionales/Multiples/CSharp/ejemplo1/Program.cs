8bf040bf3fdbe90c0231757d4e064b7a761553b1
leonardochavez <leonardo.chavroj@gmail.com>
2025-09-23 22:09:40 -0500

    02_//Condicionales//Multiples//CSharp >> Elimimando ejercicio invalido y agragando el ejercicio1

---
+++ 02_EstructurasControl/Condicionales/Multiples/CSharp/ejemplo1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Menu de opciones:");
+            Console.WriteLine("1. Sumar");
+            Console.WriteLine("2. Restar");
+            Console.WriteLine("3. Multiplicar");
+            Console.WriteLine("4. Dividir");
+            Console.Write("Escriba su opción: ");
+
+            int menu = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if (menu >= 1 && menu <= 4)
+            {
+
+                Console.Write("Ingrese el 1er numero: ");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el 2do numero: ");
+                int num2 = int.Parse(Console.ReadLine());
+
+
+                switch (menu)
+                {
+                    case 1:
+                        Console.WriteLine($"Resultado: {num1 + num2}");
+                        break;
+
+                    case 2:
+                        Console.WriteLine($"Resultado: {num1 - num2}");
+                        break;
+
+                    case 3:
+                        Console.WriteLine($"Resultado: {num1 * num2}");
+                        break;
+
+                    case 4:
+                        if (num2 != 0)
+                        {
+                            Console.WriteLine($"Resultado: {(double)num1 / num2}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se puede dividir entre 0");
+                        }
+
+                        break;
+                }  
+            }
+
+            else
+            {
+                Console.WriteLine("Opcion no valida");
+            }
+
+            Console.ReadKey();
+        }   
+    }
+}
--- 02_EstructurasControl/Condicionales/Multiples/CSharp/ejemplo1/Properties/AssemblyInfo.cs
+++ 02_EstructurasControl/Condicionales/Multiples/CSharp/ejemplo1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("Ejercico2")]
+[assembly: AssemblyTitle("ejemplo1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ejercico2")]
+[assembly: AssemblyProduct("ejemplo1")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("557ee315-9372-4303-807c-157178a2be47")]
+[assembly: Guid("b1fbe918-40de-4571-8353-68d9d8a19031")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
