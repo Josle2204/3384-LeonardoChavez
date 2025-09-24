d8139a512ef49014929a8bfa58727cd2ba383af3
leonardochavez <leonardo.chavroj@gmail.com>
2025-09-23 22:52:06 -0500

    02_//Condicionales//Multiples//CSharp//ejemplo2 >> ejemplo2 del CSharp subido

---
+++ 02_EstructurasControl/Condicionales/Multiples/CSharp/ejemplo2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calificaciones:");
+            Console.WriteLine("A > Excelente");
+            Console.WriteLine("B > Notable");
+            Console.WriteLine("C > Aprobado");
+            Console.WriteLine("D > Insuficiente");
+            Console.WriteLine("F > Reprobatorio");
+            
+            Console.WriteLine("Ingrese una letra de calificación: ");
+            string nota = Console.ReadLine().ToUpper();
+
+            if (nota == "A")
+            {
+                Console.WriteLine("Su nota es Excelente");
+            }
+            else if (nota == "B")
+            {
+                Console.WriteLine("Su nota es Notable");
+            }
+            else if (nota == "C")
+            {
+                Console.WriteLine("Su nota es Aprobado");
+            }
+            else if (nota == "D")
+            {
+                Console.WriteLine("Su nota es Insuficiente");
+            }
+            else if (nota == "F")
+            {
+                Console.WriteLine("Su nota es Reprobatorio");
+            }
+            else
+            {
+                Console.WriteLine("Opcion no valida");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
