4eee1b05fc601ad001174f80fe9c4891f4b69196
leonardochavez <leonardo.chavroj@gmail.com>
2025-10-01 23:42:04 -0500

    02_//Repetitivas//For//CSharp//ejemplo1 y ejemplo2 >> Agregando los ejemplos 1 y 2 de CSharp

---
+++ 02_EstructurasControl/Repetitivas/For/CSharp/ejemplo1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tabla de multiplicar del 1 al 12
+
+            Console.WriteLine("Escriba un numero, escribiremos su tabla de multiplicar");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine($"{num} x {i} = {num * i}");
+            }
+
+            Console.WriteLine("Se mostro sus 12 multiplicaciones");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02_EstructurasControl/Repetitivas/For/CSharp/ejemplo2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese la cantidad de numeros a contar N");
+            int n = int.Parse(Console.ReadLine());
+            int par = 0; int impar = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    par = par + 1;
+                }
+                
+                else
+                {
+                    impar = impar + 1;
+                }
+            }
+
+            Console.WriteLine($"Hay {par} numeros pares");
+            Console.WriteLine($"Hay {impar} numeros impares");
+
+            Console.ReadKey();
+        }
+    }
+}
